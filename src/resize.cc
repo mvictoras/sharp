5b67743388a5fc4a8878798f3768dddd41a83e0b
Victor Mateevitsi <mvictoras@gmail.com>
2015-04-06 16:47:21 -0500

    Testing extensions

--- src/resize.cc
+++ src/resize.cc
@@ -810,7 +810,7 @@ class ResizeWorker : public NanAsyncWorker {
         int extensionIndex = baton->output.find_last_of(".");
         std::string filename_no_extension = baton->output.substr(0, extensionIndex);
           std::cout << filename_no_extension << std::endl;
-        if (vips_dzsave(image, filename_no_extension.c_str(), "strip", !baton->withMetadata,
+        if (vips_dzsave(image, baton->output.c_str(), "strip", !baton->withMetadata,
             "tile_size", baton->tileSize, "overlap", baton->tileOverlap, NULL)) {
           return Error();
         }
